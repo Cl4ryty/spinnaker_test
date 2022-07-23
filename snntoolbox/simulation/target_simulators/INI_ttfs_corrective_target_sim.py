26e66b5eb57c9ef2df7062cc26d5f5c2a1b38f09
Cl4ryty <61427823+Cl4ryty@users.noreply.github.com>
2022-07-23 06:34:55 +0200

    saving space on spinnaker

--- snntoolbox/simulation/target_simulators/INI_ttfs_corrective_target_sim.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""INI simulator with time-to-first-spike code and corrective spikes.
-
-@author: rbodo
-"""
-
-# noinspection PyUnresolvedReferences
-from snntoolbox.simulation.target_simulators.\
-    INI_temporal_mean_rate_target_sim import SNN
